c49aa51b06cfcf6eb296fa60fe166b68717356ac
manohar <manumanohar1527@gmail.com>
2020-03-31 11:47:21 -0700

    added randomised logic

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ button.addEventListener('click', function () {
         guess.style.display = 'block'
         let random = Math.floor(Math.random() * array.length);
         message.innerHTML = array[random];
+        randomArray(array, random);
     }
 });
+
+function randomArray(arr, rand) {
+    console.log(arr);
+    console.log(rand);
+    let randomised = arr[rand].split('');
+    console.log(randomised);
+    let randomisedNumber;
+    let temp = [];
+    for (let i=0 ;i<randomised.length;i++) {
+         randomisedNumber = Math.floor(Math.random() * (randomised.length));
+         temp[i] = randomised[randomisedNumber];
+         randomised[i] = temp[i];
+    }
+
+   console.log(randomised);
+
+}
