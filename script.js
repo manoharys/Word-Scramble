7720bdc3661877e18a9e8c652010269c4bfca636
manohar <manumanohar1527@gmail.com>
2020-03-31 11:22:29 -0700

    added randomised array

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const message = document.querySelector('.message');
 const guess = document.querySelector('input');
 const button = document.querySelector('button');
+const array = ['hello', 'html', 'give', 'ran', 'big', 'word', 'toy', 'war'];
+
 
 button.addEventListener('click', function () {
     let inplay = false;
     if (!inplay) {
         inplay = true;
         button.innerHTML = "Guess";
+        button.style.padding = "9px";
+        button.style.width = '9rem';
+        guess.style.display = 'block'
+        let random = Math.floor(Math.random() * array.length);
+        message.innerHTML = array[random];
     }
 });
