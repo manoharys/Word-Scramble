ebd6ac1b2fa03f9101cfd2e5ca00233b49b35bd4
manohar <manumanohar1527@gmail.com>
2020-04-02 08:46:47 -0700

    added player guess cheker functionality

--- script.js
+++ script.js
@@ -2,39 +2,61 @@ const message = document.querySelector('.message');
 const guess = document.querySelector('input');
 const button = document.querySelector('button');
 const array = ['hello', 'html', 'give', 'ran', 'big', 'word', 'toy', 'war'];
-
-
+let random;
+let inplay = false;
+let scramble, scrambled;
+let randomised
 button.addEventListener('click', function () {
-    let inplay = false;
+    message.style.color = "black";
     if (!inplay) {
         inplay = true;
         button.innerHTML = "Guess";
         button.style.padding = "9px";
-        button.style.width = '9rem'; 
-        guess.style.display = 'block' 
-        let random = Math.floor(Math.random() * array.length);
-        message.innerHTML = array[random];
-        randomArray(array, random);
+        button.style.width = '9rem';
+
+        guess.style.display = 'block'
+        random = Math.floor(Math.random() * array.length);
+        scramble = array[random];
+        
+        scrambled = randomArray(array, random);
+        console.log(scramble);
+        console.log(scrambled);
+        message.innerHTML = scrambled;
+    } else {
+        let tempGuess = guess.value;
+        console.log(tempGuess);
+        if (tempGuess === scramble) {
+            inplay = false;
+            console.log("hello");
+            message.style.color = "green";
+            button.innerHTML = "Start";
+            guess.style.display = "none";
+            message.innerHTML = "correct!!"
+           
+        } else {
+            message.innerHTML = "wrong " + scrambled;
+        }
     }
 });
 
 function randomArray(arr, rand) {
-    console.log(arr);
+    //console.log(arr);
     //console.log(arr.length);
-    console.log(rand);
-    let randomised = arr[rand].split('');
-    console.log(randomised);
-    let i = randomised.length,temp,j;
-    
+   // console.log(rand);
+     randomised = arr[rand].split('');
+  //  console.log(randomised);
+    let i = randomised.length;
+  let temp, j;
+
     //Fisher-Yates Shuffle algorithm
 
-    while(--i>0){
-        j = Math.floor(Math.random()*(i+1));
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
         //console.log(randomNumber);
         temp = randomised[i];
         randomised[i] = randomised[j];
         randomised[j] = temp;
     }
-  console.log(randomised);
+    return (randomised.join(""));
 
 }
